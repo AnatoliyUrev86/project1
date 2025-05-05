e73a94843713fd01675c2d10ea10bbb4540b4224
AnatoliyUrev86 <alexurev75@gmail.com>
2025-05-05 20:47:32 +0300

    commit

--- js/task-6.js
+++ js/task-6.js
@@ -2,7 +2,39 @@
 
 // Напиши скрипт який виводить телефон та номер користувача. Вивксти у консоль імя та телефон
 
-const names = "jacob,William";
-const phones = "380977775566,380683332299";
-const nameArr = names.split(",");
-console.log(nameArr);
+// const names = "jacob,William";
+// const phones = "380977775566,380683332299";
+// const nameArr = names.split(",");
+// console.log(nameArr);
+
+// До модуля 6 кінець
+
+//Якщо наша функція викликається не в суворому режимі то у консолі ми отримаємо Window;
+// Якщо в суворому режимі "use strict" то undefined
+
+// const objC = { c: "objC" };
+// Метод Object.create(obj) створює і повертає новий об'єкт, зв'язуючи його з об'єктом obj
+// const objB = Object.create(objC);
+// objB кажемо твоїм прототипом буде objC за допомогою Object.create
+// objB.b = "objB";
+
+// const objA = Object.create(objB);
+// objА кажемо твоїм прототипом буде objВ за допомогою Object.create
+// objA.a = "objA";
+// objA.a - об'єкт А. з ключем а
+// console.log(objA);
+// console.log(objB);
+// console.log(objA.c);
+
+// Задача
+
+// Написати програмне забезпечення для авто
+// Створи об'єкт з методами за швидкість за модель
+
+// const cruiseControl = {
+//     speed: 0,
+//     brand: "Audi",
+//     acclerate() {
+
+//     }
+// }
--- js/task-7.js
+++ js/task-7.js
@@ -1,37 +1,42 @@
 // МОДУЛЬ 6
 // "use strict";
-function add() {
-  console.log(this);
-}
+// function add() {
+//   console.log(this);
+// }
 // this - функція отримує під час виклику і бере його з того об'єкта який стоїть з ліва в данному випадку
 // з ліва від add(); нічого не має тому ми посилаємось на нашу глобальну сутність Window
-add();
-//Якщо наша функція викликається не в суворому режимі то у консолі ми отримаємо Window;
-// Якщо в суворому режимі "use strict" то undefined
+// add();
 
-// const objC = { c: "objC" };
-// Метод Object.create(obj) створює і повертає новий об'єкт, зв'язуючи його з об'єктом obj
-// const objB = Object.create(objC);
-// objB кажемо твоїм прототипом буде objC за допомогою Object.create
-// objB.b = "objB";
+// const user = {
+//   userName: "Alise",
+//   showThis() {
+//   showThis() - метод створений в середині об'єкта user
+//     console.log(this);
+//   },
+// };
+// user.showThis() - для того щоб викликати метод showThis() ми звертаємось до нашого об'єкта user
+// user.showThis();
+//  this - отримує посилання на  під час виклику на об'єкта який стоїть з ліва user
 
-// const objA = Object.create(objB);
-// objА кажемо твоїм прототипом буде objВ за допомогою Object.create
-// objA.a = "objA";
-// objA.a - об'єкт А. з ключем а
-// console.log(objA);
-// console.log(objB);
-// console.log(objA.c);
+// const user = {
+//   userName: "Alise",
+//   showThis() {
+//     console.log("showThis", this);
+//     const add = () => {
+//       console.log("add", this);
+// this в стрілочній функції візьме область видимості із батьківського елементу тобто з методу  showThis
+//     };
+//     add();
+//   },
+// };
 
-// Задача
 
-// Написати програмне забезпечення для авто
-// Створи об'єкт з методами за швидкість за модель
 
-// const cruiseControl = {
-//     speed: 0,
-//     brand: "Audi",
-//     acclerate() {
 
-//     }
-// }
+
+
+
+
+
+
+
