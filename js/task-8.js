401ea42a4bed9491756771377bd61f520cc04049
AnatoliyUrev86 <alexurev75@gmail.com>
2025-05-10 08:23:05 +0300

    commit

--- js/task-8.js
+++ js/task-8.js
@@ -1,9 +1,14 @@
 // ПРОТОТИПИ
 
-const animal = {
-  legs: 4,
-};
+// const animal = {
+//   legs: 4,
+// };
 
-const dog = Object.create(animal);
-dog.name = "Patron";
-consolr.legs(dog);
+// const dog = Object.create(animal);
+// dog.name = "Patron";
+// consolr.legs(dog);
+
+// МОДУЛЬ 7
+
+// Для того щоб отримати кнопу із html файлу потрібно
+
