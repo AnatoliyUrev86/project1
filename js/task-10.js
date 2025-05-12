96b946e774cafa16989b795447dd2cd52a39cddd
AnatoliyUrev86 <alexurev75@gmail.com>
2025-05-12 12:59:33 +0300

    commit

--- js/task-10.js
+++ js/task-10.js
@@ -16,7 +16,7 @@ console.log("ok");
 // });
 // Замість () => ми передаємо посилання на звнішню фунцію handleClick - як 2-й аргумент
 btn.addEventListener("click", handleClick);
-
+box.addEventListener("click", handleClick);
 // Також можемо створити зовнішю функцію
 // Тепер ми хочемо щоб при натисканні на кнопку значення числове збільшувалось на 50 для цього
 let step = 0;
@@ -26,6 +26,24 @@ function handleClick() {
   // В середині нашої функції звертаємось до нашої змінної і кажемо збільшуй значення після
   // натискання на 50 тобто  step += 50;
   step += 50;
-    //   console.log(step);
-    // Тепер ми х
+  //   console.log(step);
+  // Тепер ми хочемо щоб при натисканні на кнопку наша кнопка рухалась
+  box.style.marginTop = `${step}px`;
+  // Звертаємось до нашого  box крапка властивість style потім marginTop - рухатись в верх
+  // Задаємо значення через шаблонні рядки  `${step}px` звертаючись до  step += 50 за фігуні дужки px
+  box.style.marginLeft = `${step}px`;
 }
+
+// РОБОТА З <input>
+
+const checkbox = document.querySelector(".js-checkbox");
+// checkbox - це маленький квадратик який біля нашого рядка <input> на який ми ставимо галочку і у консолі
+// буде відображатись "ok"- значення яке присвоїли
+// Тут ми звертаємось до checkbox і викликаємо подію яка нам потрібна
+// Коли ми тиснемо на checkbox відпрацьовує подія "change"
+checkbox.addEventListener("change", () => {
+  // () => - стрілочна функція буде відпрацьовувати в момент натискання на checkbox
+  console.log("ok");
+});
+
+const userName = document.querySelector(".js-user-mane");
