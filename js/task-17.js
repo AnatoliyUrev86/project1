86b7634a071d4b54bffed4a0efbf8010827c2d03
AnatoliyUrev86 <alexurev75@gmail.com>
2025-06-10 20:49:49 +0300

    commit

---
+++ js/task-17.js
@@ -0,0 +1,4 @@
+// МОДУЛЬ 11 ПЕРШЕ ЗАНЯТТЯ
+// Для того щоб зробити запит на сервер використовуємо метод fetch()
+console.log(fetch("http://jsonplaceholder.typicode.com/todos"));
+// В цей метод як аргумент передається URL "http://jsonplaceholder.typicode.com/todos" за яким ми хочемо зробити запит
