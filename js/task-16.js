1b057084dd3f5003176a023ce69359fe0c9802e9
AnatoliyUrev86 <alexurev75@gmail.com>
2025-06-10 20:26:58 +0300

    commit

--- js/task-16.js
+++ js/task-16.js
@@ -273,3 +273,19 @@ function handalClick() {
 // і до індекса додаємо одиницю щоб відображення починалося не з першого індекса а не з нулевого
 // Після того ми маємо виводити повідомлення виграв наш гравець чи програв для цього в нашому  setTimeout
 // пишемо  result.textContent = isWinner ? "Winner" : "Luser"
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    // Data handling
+    console.log("Отримані дані:", data);
+  })
+  .catch((error) => {
+    // Error handling
+    console.log("Помилка запиту:", error);
+  });
